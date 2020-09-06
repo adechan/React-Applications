baf146db75266fb1b7755331a7dd3911a76812d7
adechan <ade.enter@yahoo.com>
2020-09-06 18:47:04 +0300

    Using hooks to set the posts

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import instagramText from './images/instagram.png';
 import './App.css';
+import Post from './Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "Ade",
+      caption: "WOW it works!",
+      imageUrl: "https://reactjs.org/logo-og.png"
+    },
+    {
+      username: "Doggy",
+      caption: "Great image!",
+      imageUrl: "https://firebase.google.com/images/social.png"
+    }
+  ]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <div className="app__header">
+        <img 
+          className="app__headerImage"
+          src={instagramText}
+          alt=""
+        />
+      </div>
+
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }  
+
     </div>
   );
 }
