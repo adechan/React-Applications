85fdb50b619129df13281a179a2c2f600071f146
adechan <ade.enter@yahoo.com>
2020-10-11 14:13:27 +0300

    Updates on UI design

--- discord-clone/src/App.js
+++ discord-clone/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-     {/* Sidebar */}
-     <Sidebar />
-     {/* Char */}
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
